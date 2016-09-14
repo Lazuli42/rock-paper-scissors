6561e4a256dec8ad69f5cdd0d453cacadb40d15f
anand angalig and zachary baum <anand@>
2016-09-14 14:18:18 -0400

    finish the front end feature for 2 players

--- app/app.php
+++ app/app.php
@@ -2,6 +2,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RPS.php";
 
+    session_start();
+    if (empty($_SESSION["selections"])) {
+      $_SESSION["selections"] = "";
+    }
+
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
@@ -15,9 +20,16 @@
     });
 
     $app->get("/p1_select", function() use($app) {
-        $rps = new RPS($_GET["p1Selection"]);
-        
+        $rps = new RPS($_GET["p1Selection"], "");
+        $rps->save();
         return $app["twig"]->render("two_players.html.twig");
+    });
+
+    $app->get("/p2_select", function() use($app) {
+        $rps = RPS::getSelections();
+        $rps->setPlayer2($_GET["p2Selection"]);
+        $rps->playGame();
+        return $app["twig"]->render("two_players.html.twig", array("rps" => $rps));
     });
 
     return $app;
--- src/RPS.php
+++ src/RPS.php
@@ -1,9 +1,9 @@
 <?php
     class RPS
     {
-        public $player1;
-        public $player2;
-        public $winner;
+        private $player1;
+        private $player2;
+        private $winner;
 
         function __construct($player1, $player2)
         {
@@ -11,9 +11,24 @@
             $this->player2 = $player2;
         }
 
+        function setPlayer2($new_player2)
+        {
+            $this->player2 = (string) $new_player2;
+        }
+
         function getWinner()
         {
             return $this->winner;
+        }
+
+        function save()
+        {
+         $_SESSION["selections"] = $this;
+        }
+
+        static function getSelections()
+        {
+          return $_SESSION["selections"];
         }
 
         function playGame()
@@ -24,32 +39,39 @@
 //=========================Player1===============================
             if (($player1 === "rock") && ($player2 === "scissors"))
             {
+                $this->winner = "Player 1";
                 return "Player 1";
             }
             if (($player1 === "paper") && ($player2 === "rock"))
             {
+                $this->winner = "Player 1";
                 return "Player 1";
             }
             if (($player1 === "scissors") && ($player2 === "paper"))
             {
+                $this->winner = "Player 1";
                 return "Player 1";
             }
 //=========================Player2===============================
             if (($player2 === "rock") && ($player1 === "scissors"))
             {
+                $this->winner = "Player 2";
                 return "Player 2";
             }
             if (($player2 === "paper") && ($player1 === "rock"))
             {
+                $this->winner = "Player 2";
                 return "Player 2";
             }
             if (($player2 === "scissors") && ($player1 === "paper"))
             {
+                $this->winner = "Player 2";
                 return "Player 2";
             }
 //===========================Draw===================================
             if ($player1 === $player2)
             {
+                $this->winner = "Draw!";
                 return "Draw!";
             }
         }
